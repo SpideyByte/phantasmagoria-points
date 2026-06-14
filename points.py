29e1a0e18876b96294e29129bdf1ecc2b59fccaf
SpideyByte <212452678+SpideyByte@users.noreply.github.com>
2026-06-14 15:47:58 +1000

    Added test and added more info to help

--- points.py
+++ points.py
@@ -18,6 +18,8 @@ async def on_ready():
 
 @commands.command()
 async def points(ctx, type, user: discord.Member = None, amount: int = 0):
+    if type == "test":
+    	await ctx.send("Online!")
     if type == "unregister":
     	if ctx.author.id == 1258323457674838028 or ctx.author.id == 1224550701259030598 or ctx.author.id == 1409793395555307541:
     		data = load_data()
@@ -37,7 +39,7 @@ async def points(ctx, type, user: discord.Member = None, amount: int = 0):
 	    		open_account(member.id, data)
 	    		save_data(data)
     if type == "help":
-    	await ctx.send("help \nShows the list of subcommands and their definitions. Usable by all.\n\nshow\nShows you your current point balance. Usable by all.\n\nmodify\nChanges the amount of points someone has, by default the target user is you. Usable by Hallowxity and Soup.\n\nleaderboard\nShows a leaderboard containing everyone registered in it. This command takes a while due to it looking up the user associated with everyone registered's ID. Usable by all.\n\nregister\nRegisters the whole server. Usable by all.\n\nunregister\nUnregisters the target user from the system, used if people leave the developer team. Usable by Hallowxity and Soup.\n\nThe structure of $points is '$points <subcommand> <user> <amount>'. User and amount are optional, although amount is required for modify and user is required for modify and unregister.")
+    	await ctx.send("help \nShows the list of subcommands and their definitions. Usable by all.\n\nshow\nShows you your current point balance. Usable by all.\n\nmodify\nChanges the amount of points someone has, by default the target user is you. Usable by Hallowxity and Soup.\n\nleaderboard\nShows a leaderboard containing everyone registered in it. This command takes a while due to it looking up the user associated with everyone registered's ID. Usable by all.\n\nregister\nRegisters the whole server. Usable by all.\n\nunregister\nUnregisters the target user from the system, used if people leave the developer team. Usable by Hallowxity and Soup.\n\ntest\nTells you if the bot is online, if it is it will respond with 'Online!.' Otherwise it doesn't respond at all. Usable by all.\n\ndebug\nSays the current contents of points_eco.json. (The file containing the points data.) Usable by all.\n\nThe structure of $points is '$points <subcommand> <user> <amount>'. User and amount are optional, although amount is required for modify and user is required for modify and unregister.\n\nIf you would like to contribute to this, you can visit the Github repository [here](https://github.com/SpideyByte/phantasmagoria-points).")
     if type == "show":
     	target = user or ctx.author
     	target_ping = str(f'<@{target.id}>')
